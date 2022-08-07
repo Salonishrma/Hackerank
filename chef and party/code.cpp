20b6388d756581d73b953c63e741ac0a66ddfc3a
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-07 14:57:00 +0530

    Update code.cpp

--- chef and party/code.cpp
+++ chef and party/code.cpp
@@ -1,8 +1,23 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 Hlo }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,count=0;
+        cin>>n;
+        int array[n];
+        for(int i=0;i<n;i++){
+            cin>>array[i];
+        }
+        sort(array,array+n);
+        for(int i=0;i<n;i++){
+        if(count>=array[i]){
+            count++;
+        }
+        }
+            cout<<count<<endl;
+        
+    }
+    return 0;
+}
