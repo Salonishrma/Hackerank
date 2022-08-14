2f5f2ff930c49e7996a6f2fa33ca2787f7ba6bf9
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-14 23:33:40 +0530

    Update code.cpp

--- Simple array sum/code.cpp
+++ Simple array sum/code.cpp
@@ -1,6 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int array[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>array[i];
+    }
+    for(int i=0;i<n;i++)
+        sum+=array[i];
+        cout<< sum << endl;
+    
+
+return 0;
 }
