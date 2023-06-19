d54848abdfed5b6d7377f67e31993e20806f57a2
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-06-19 22:36:09 +0530

    Update code .cpp

--- stl/Vector-Erase/code .cpp
+++ stl/Vector-Erase/code .cpp
@@ -1,6 +1,32 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int n;
+   cin>>n;
+   vector<int>v1;
+   for(int i=0;i<n;i++){
+       int x;
+       cin>>x;
+       v1.push_back(x);
+   }
+   int m;
+   cin>>m;
+   m--; //1 based indexing
+   v1.erase(v1.begin()+m);
+   int l,s;
+   cin>>l>>s;
+   l--;
+   s--;
+   v1.erase(v1.begin()+l,v1.begin()+s);
+   cout<<v1.size()<<endl;
+   for(int i=0;i<v1.size();i++){
+       cout<<v1[i]<<" ";
+   }
+    return 0;
 }
